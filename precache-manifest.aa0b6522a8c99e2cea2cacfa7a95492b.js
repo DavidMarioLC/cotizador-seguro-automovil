549042db921f24b18c13c61f7d5da613521ee4a5
David Mario <davidmariolc.dev@gmail.com>
2020-08-10 23:18:25 -0500

    Updates

---
+++ precache-manifest.aa0b6522a8c99e2cea2cacfa7a95492b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827083bedf5807b818999e8f7e32c34f",
+    "url": "/cotizador-seguro-automovil/index.html"
+  },
+  {
+    "revision": "0fe202154c55db3ecc0c",
+    "url": "/cotizador-seguro-automovil/static/css/main.91522dd1.chunk.css"
+  },
+  {
+    "revision": "24b4745c57db5d4e5713",
+    "url": "/cotizador-seguro-automovil/static/js/2.89f58101.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizador-seguro-automovil/static/js/2.89f58101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe202154c55db3ecc0c",
+    "url": "/cotizador-seguro-automovil/static/js/main.e8f8f559.chunk.js"
+  },
+  {
+    "revision": "f3a3c451dc40dd9ecbe9",
+    "url": "/cotizador-seguro-automovil/static/js/runtime-main.2ae873d7.js"
+  }
+]);
